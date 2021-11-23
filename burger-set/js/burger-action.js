44dfa2b4615520bd306cafc99bf94d1127597afc
gaponova <dzensmart@gmail.com>
2021-11-23 14:55:26 +0200

    Сделали burger-set

---
+++ burger-set/js/burger-action.js
@@ -0,0 +1,41 @@
+/*Появление адаптивного меню*/
+
+//Вариант 1
+$(document).ready(function () {
+  $('.burger-1').click(function (event) {
+    $('.burger-1, .header__menu').toggleClass('active');
+    // $('body').toggleClass('lock');
+  });
+});
+
+//Вариант 2
+$(document).ready(function () {
+  $('.burger-2').click(function (event) {
+    $('.burger-2, .header__menu').toggleClass('active');
+    // $('body').toggleClass('lock');
+  });
+});
+
+//Вариант 3
+$(document).ready(function () {
+  $('.burger-3').click(function (event) {
+    $('.burger-3, .header__menu').toggleClass('active');
+    // $('body').toggleClass('lock');
+  });
+});
+
+//Вариант 4
+$(document).ready(function () {
+  $('.burger-4').click(function (event) {
+    $('.burger-4, .header__menu').toggleClass('active');
+    // $('body').toggleClass('lock');
+  });
+});
+
+//Вариант 5
+$(document).ready(function () {
+  $('.burger-5').click(function (event) {
+    $('.burger-5, .header__menu').toggleClass('active');
+    // $('body').toggleClass('lock');
+  });
+});
